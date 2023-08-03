0b6cd36b7a7fb65cda663d4ae7cc086568e51166
Oleksii Kotvytskyi <kot.aleksei43@gmail.com>
2023-08-03 13:40:17 +0300

    feat: add prisma db

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
+import prismadb from "@/lib/prisma-db";
 
 const inter = Inter({ subsets: ["latin"] });
 
