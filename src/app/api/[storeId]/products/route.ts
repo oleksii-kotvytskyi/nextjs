e38cbc1eb1f84297580b2fce85e2f77fb4b02c9c
Oleksii Kotvytskyi <kot.aleksei43@gmail.com>
2023-09-18 15:09:23 +0300

    fix: cors error

--- src/app/api/[storeId]/products/route.ts
+++ src/app/api/[storeId]/products/route.ts
@@ -3,6 +3,16 @@ import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prisma-db";
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
 export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
@@ -126,7 +136,7 @@ export async function GET(
       },
     });
 
-    return NextResponse.json(products);
+    return NextResponse.json(products, { headers: corsHeaders });
   } catch (error) {
     console.log("[PRODUCTS_GET]", error);
 
